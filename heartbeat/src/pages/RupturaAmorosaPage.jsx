aa1e2cfaff4007f2a2e8fd5c9eca247eca959916
Katherine Rueda <kathkien24@gmail.com>
2024-10-07 23:11:19 -0600

    sexto commit

--- heartbeat/src/pages/RupturaAmorosaPage.jsx
+++ heartbeat/src/pages/RupturaAmorosaPage.jsx
@@ -1,10 +1,25 @@
-// En src/pages/RupturaAmorosaPage.js
 import React from 'react';
-import { Typography, Container, Paper } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { Typography, Container, Paper, Button } from '@mui/material';
 
 const RupturaAmorosaPage = () => {
+  const navigate = useNavigate();
+
+  const handlePrev = () => {
+    navigate('/categories/baja-autoestima');  // Cambia esto según la categoría previa que quieras mostrar
+  };
+
+  const handleNext = () => {
+    navigate('/categories/perdi-a-mi-mejor-amigo');  // Cambia esto por la próxima categoría que quieras mostrar
+  };
+
+  const goBack = () => {
+    navigate('/categories');  // Regresar al listado principal de categorías
+  };
+
   return (
     <Container maxWidth="md">
+      <Button variant="contained" onClick={goBack}>Regresar</Button>
       <Paper style={{ padding: '20px', marginTop: '20px' }}>
         <Typography variant="h4" gutterBottom>Ruptura amorosa</Typography>
         <Typography paragraph>
@@ -12,6 +27,8 @@ const RupturaAmorosaPage = () => {
         </Typography>
         {/* Más contenido y elementos como imágenes, listas, etc. */}
       </Paper>
+      <Button variant="contained" onClick={handlePrev} style={{ marginRight: '10px' }}>Anterior</Button>
+      <Button variant="contained" onClick={handleNext}>Siguiente</Button>
     </Container>
   );
 };
