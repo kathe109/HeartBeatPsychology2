a08c4abbc34b6ed63adcd578a95a5c0b5220a3b1
Katherine Rueda <kathkien24@gmail.com>
2024-10-07 21:05:23 -0600

    cuarto commit

--- heartbeat/src/App.jsx
+++ heartbeat/src/App.jsx
@@ -1,30 +1,46 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import AuthForm from './components/AuthForm';
 import Footer from './components/Footer';
+import { Container } from '@mui/material';
 import AboutUs from './pages/About';
 import Sessions from './pages/Sessions';
 import CategoriesPage from './pages/CategoriesPage';
 import CategoryDetailPage from './pages/CategoryDetailPage';
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  return (
-    <Router>
-      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
-      <Routes>
-        <Route path="/" element={<AuthForm setIsLoggedIn={setIsLoggedIn} />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/sessions" element={<Sessions />} />
-        <Route path="/login" element={<AuthForm setIsLoggedIn={setIsLoggedIn} />} />
-        <Route path="/categories" element={<CategoriesPage />} />
-        <Route path="/categories/:categoryId" element={<CategoryDetailPage />} />
-      </Routes>
-      <Footer />
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                {/* Ruta contenedora con Navbar y Footer para páginas seleccionadas */}
+                <Route element={<MainLayout isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />}>
+                    <Route path="/" element={<AuthForm setIsLoggedIn={setIsLoggedIn} />} />
+                    <Route path="/login" element={<AuthForm setIsLoggedIn={setIsLoggedIn} />} />
+                    <Route path="/categories" element={<CategoriesPage />} />
+                </Route>
+                {/* Rutas sin Navbar o Footer */}
+                <Route path="/about" element={<AboutUs />} />
+                <Route path="/sessions" element={<Sessions />} />
+                <Route path="/categories/:categoryId" element={<CategoryDetailPage />} />
+            </Routes>
+        </Router>
+    );
 };
+
+// Componente de layout principal que incluye Navbar y Footer
+function MainLayout({ isLoggedIn, setIsLoggedIn }) {
+    return (
+        <>
+            <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+            <Container component="main" maxWidth="xs">
+                <Outlet /> {/* Este Outlet renderizará los componentes de las rutas hijas */}
+            </Container>
+            <Footer />
+        </>
+    );
+}
 
 export default App;
