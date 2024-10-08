66b47a5396ba99ab61744cee89a27fca019f0e91
Katherine Rueda <kathkien24@gmail.com>
2024-10-07 20:52:45 -0600

    tercer commit

--- heartbeat/src/App.jsx
+++ heartbeat/src/App.jsx
@@ -1,39 +1,30 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import AuthForm from './components/AuthForm';
 import Footer from './components/Footer';
-import { Container } from '@mui/material';
-import AboutUs from './pages/About';  
-import Sessions from './pages/Sessions'; 
+import AboutUs from './pages/About';
+import Sessions from './pages/Sessions';
 import CategoriesPage from './pages/CategoriesPage';
 import CategoryDetailPage from './pages/CategoryDetailPage';
 
 const App = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false); 
-  
-    return (
-        <Router>
-            <Routes>
-                {/* Ruta contenedora para las páginas que incluyen Navbar y Footer */}
-                <Route element={<>
-                    <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
-                    <Container component="main" maxWidth="xs">
-                        <Outlet />  {/* Este Outlet renderizará los componentes de las rutas hijas */}
-                    </Container>
-                    <Footer />
-                </>}>
-                    <Route path="/about" element={<AboutUs />} />
-                    <Route path="/sessions" element={<Sessions />} />
-                    <Route path="/login" element={<AuthForm setIsLoggedIn={setIsLoggedIn} />} />
-                    <Route path="/categories" element={<CategoriesPage />} />
-                </Route>
-                
-                {/* Ruta para la página de detalle de categoría sin Navbar o Footer */}
-                <Route path="/categories/:categoryId" element={<CategoryDetailPage />} />
-            </Routes>
-        </Router>
-    );
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <Router>
+      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+      <Routes>
+        <Route path="/" element={<AuthForm setIsLoggedIn={setIsLoggedIn} />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/sessions" element={<Sessions />} />
+        <Route path="/login" element={<AuthForm setIsLoggedIn={setIsLoggedIn} />} />
+        <Route path="/categories" element={<CategoriesPage />} />
+        <Route path="/categories/:categoryId" element={<CategoryDetailPage />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 };
 
 export default App;
